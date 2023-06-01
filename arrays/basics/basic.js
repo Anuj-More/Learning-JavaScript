dfaa53bc416e4275771f02cb6cfba507fc321ea9
Anuj More <work.anujmore@gmail.com>
2023-06-01 23:28:17 +0530

    Started array with basic functions and stuff

---
+++ arrays/basics/basic.js
@@ -0,0 +1,14 @@
+
+let fruits = ["apple", "mango", "banana"];
+
+fruits.push("guava");      //adds elements to the end
+fruits.pop();              //removes an element from the end
+fruits.unshift("chikoo");  //adds element in the beginning
+fruits.shift();            //removes element from the beginning  
+// console.log(fruits);
+
+let length = fruits.length;
+// console.log(length);
+
+let index = fruits.indexOf("kiwi");
+// console.log(index);
