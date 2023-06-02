23ba8e7efc68dfa505f5dd9f979b0f2111ce63b5
Anuj More <work.anujmore@gmail.com>
2023-06-02 20:20:04 +0530

    Added another usecase of spread operator

--- arrays/spread operator/spread.js
+++ arrays/spread operator/spread.js
@@ -9,3 +9,13 @@ let numbers = [1,2,3,4,5,6,7,8,9];
 let max = Math.max(...numbers);     //unpacked the elements in the array
 
 console.log(max);
+console.log(...numbers);
+
+
+//another usecase:
+
+let class1 = ["name1", "name2", "name3"];
+let class2 = ["name4", "name5", "name6"];
+
+class1.push(...class2);     //added elements of class1 to class2
+console.log(...class1);     //one by one after unpacking
