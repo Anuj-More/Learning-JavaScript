51d3c2bbfab273bb4ae83f9c0d676551bc2df594
Anuj More <work.anujmore@gmail.com>
2023-06-02 19:48:36 +0530

    Sorting arrays

---
+++ arrays/sort/sort.js
@@ -0,0 +1,15 @@
+//there exists .sort() method to sort any kind of arrays in JS
+
+let prices = [1,4,3,6,7,2,8,9];
+let places = ["home", "work", "office", "garden", "terrace"];
+
+prices = prices.sort();                 //to sort in ascending order
+places = places.sort().reverse();       // to sort in descending order
+
+
+for(let price of prices){
+    console.log(price);
+}
+for(let place of places){
+    console.log(place);
+}
