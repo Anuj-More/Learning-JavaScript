f065a8825506ade4ce77be88528e815e4c17eefe
Anuj More <work.anujmore@gmail.com>
2023-06-07 15:14:04 +0530

    Added my practice code of the clock program as comment

--- clock program/clock.js
+++ clock program/clock.js
@@ -27,3 +27,35 @@ function update(){
         return time.length < 2 ? "0" + time : time;
     }
 }
+
+// My Practice Code
+
+// let mylabel = document.getElementById("mylabel");
+
+// update();
+// setInterval(update, 1000);
+
+// function update(){
+//     let date = new Date();
+//     mylabel.innerHTML = formatTime(date);
+
+//     function formatTime(date){
+//         let hours = date.getHours();
+//         let minutes = date.getMinutes();
+//         let seconds = date.getSeconds();
+//         let ampm = hours >= 12 ? "pm" : "am";
+
+//         hours = (hours%12 || 12);
+
+//         hours = formatZeroes(hours);
+//         minutes = formatZeroes(minutes);
+//         seconds = formatZeroes(seconds);
+
+//         function formatZeroes(data){
+//             return data < 10 ? `0${data}` : `${data}`;
+//         }
+
+//         return `${hours}:${minutes}:${seconds} ${ampm}`;
+//     }
+
+// }
