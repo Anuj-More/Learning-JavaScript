77fc4c8829be950ff8e1165516cbe49469c489ab
Anuj More <work.anujmore@gmail.com>
2023-06-07 16:55:06 +0530

    A .time() method to track time of certain operations

---
+++ console.time/time.js
@@ -0,0 +1,12 @@
+// console.time() =    Starts a timer you can use to 
+//                     track how long an operation takes.
+//                     Give each timer a unique name.
+
+//start
+console.time("Response time");
+
+// alert("CLICK");
+setTimeout(() => console.log("Hello!"), 1000)
+
+//end
+console.timeEnd("Response time");
